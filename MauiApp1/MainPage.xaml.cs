5e6664450aecce90d353cef737268b70e7e1bb14
Evert <e.scherpenborg@prognotice.nl>
2023-11-08 11:40:16 +0100

    Add project files.

---
+++ MauiApp1/MainPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿namespace MauiApp1
+{
+	public partial class MainPage : ContentPage
+	{
+
+		public MainPage()
+		{
+			InitializeComponent();
+		}
+
+		private void Switch1_Toggled(object sender, ToggledEventArgs e)
+		{
+			if(Switch1.IsToggled)
+				Switch1.ThumbColor = Colors.Blue;
+			else
+				Switch1.ThumbColor = Colors.Red;
+		}
+	}
+
+}
